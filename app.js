bacca1e92af641e5f66091f7990f3a0b8c581f67
spencerliechty <spencerliechty@gmail.com>
2013-01-23 20:47:09 -0600

    Add Register view/template/handler

--- app.js
+++ app.js
@@ -10,4 +10,26 @@ app.get('/', function(req, res) {
     res.render("index.jade", {layout:false});
 });
 
+app.get('/account/authenticated', function(req, res) {
+    if ( req.session.loggedIn) {
+        res.send(200);
+    } else {
+        res.send(401);
+    }
+});
+
+app.post('/register', function(req, res) {
+    var firstName = req.param('firstName', '');
+    var lastName = req.param('lastName','');
+    var email = req.param('email', null);
+    var password = req.param('password', null);
+    if ( null === email || null === password ) {
+        res.send(400);
+        return;
+    }
+
+    Account.register(email, password, firstName, lastName);
+    res.send(200);
+});
+
 app.listen(8008);
