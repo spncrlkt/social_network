67e7adcd079cc525cb6e8ba57a78f4abdedf8bdd
spencerliechty <spencerliechty@gmail.com>
2013-01-28 21:01:49 -0600

    Finish Chapter 6 forgot/reset password

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var MemoryStore = require('connect').session.MemoryStore;
+
+// Import the data layer
+var mongoose = require('mongoose');
+var config = {
+    mail: require('./config/mail')
+};
+
+// Import the accounts
+var Account = require('./models/Account')(config, mongoose, nodemailer);
 
 app.configure(function() {
     app.set('view engine', 'jade');
     app.use(express.static(__dirname + '/public'));
+    app.use(express.limit('1mb');
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session(
+        {secret: "SocialNet secret key", store: new MemoryStore()}));
+    mongoose.connect('mongodb://localhost/nodebackbone');
 });
 
 app.get('/', function(req, res) {
@@ -51,6 +68,38 @@ app.post('/login', function(req, res) {
         console.log('login was successful');
         res.send(200);
     });
+});
+
+app.post('/forgotpassword', function(req, res) {
+    var hostname = req.headers.host;
+    var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
+    var email = req.param('email',null);
+    if ( null === email || email.length < 1) {
+        res.send(400);
+        return;
+    }
+
+    Account.forgotPassword(email, resetPasswordUrl, function(success) {
+        if (success) {
+            res.send(200);
+        } else {
+            res.send(404);
+        }
+    });
+});
+
+app.get('/resetPassword', function(req, res) {
+    var accountId = req.param('account', null);
+    res.render('resetPassword.jade', {locals:{accountId:accountId}});
+});
+
+app.post('/resetPassword', function(req, res) {
+    var accountId = req.param('accountId', null);
+    var password = req.param('password', null);
+    if (null !== accountId && null !== password) {
+        Account.changePassword(accountId, password);
+    }
+    res.render('resetPasswordSuccess.jade');
 });
 
 
